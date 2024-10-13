abbf3136348c45241e49065ea45360a041f4884c
brandon-parkerson <brandon-m-parkerson@gmail.com>
2024-10-13 17:26:22 +0300

    Tweak caesar cipher function

--- index.js
+++ index.js
@@ -64,8 +64,7 @@ function caesarCipher(num, string) {
 
   for (let c of chars) {
     let character = c;
-
-    // Check if the character is a letter
+    // Check if character is a letter
     if (/[a-zA-Z]/.test(character)) {
       if (character === character.toUpperCase()) {
         let charToLower = character.toLowerCase();
@@ -78,18 +77,17 @@ function caesarCipher(num, string) {
         newArray.push(newChar);
       }
     } else {
-      // If it's not a letter, keep the character as is (e.g., punctuation or spaces)
       newArray.push(character);
     }
   }
-
   return newArray.join("");
 }
+
 function analyzeArray(array) {
   const sum = array.reduce((a, b) => a + b, 0);
   const avg = sum / array.length || 0;
   const min = Math.min(...array);
-  const max = Math.max(...array)
+  const max = Math.max(...array);
   const length = array.length;
 
   let object = { average: avg, min: min, max: max, length: length };
